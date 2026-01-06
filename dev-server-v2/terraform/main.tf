20f75326b68826eca298ea55c0738d4ae5141e52
sangyeon kim <greyhairchooselife@gmail.com>
2026-01-06 16:40:31 +0900

    fix: 변수 타입에 맞는 값으로 수정

--- dev-server-v2/terraform/main.tf
+++ dev-server-v2/terraform/main.tf
@@ -1,4 +1,4 @@
-data "aws_security_group" "example" {
+data "aws_security_group" "general_dev_server" {
   name = "general-dev-server"
 }
 
@@ -6,11 +6,11 @@ module "dev-server" {
   source = "./modules/ec2-instance-with-eip"
 
   ami_id            = "ami-0c447e8442d5380a3"
-  instance_count    = "5"
+  instance_count    = 5
   instance_type     = "t3.medium"
   volume_size       = 20
   key_name          = "dev-server"
-  security_group_id = data.aws_security_group.example.id
+  security_group_id = data.aws_security_group.general_dev_server.id
   tag_name          = "general_dev_server"
 }
 
@@ -18,11 +18,11 @@ module "cpu_test" {
   source = "./modules/ec2-instance-with-eip"
 
   ami_id            = "ami-0c447e8442d5380a3"
-  instance_count    = "5"
+  instance_count    = 1
   instance_type     = "c7i.xlarge"
   volume_size       = 20
   key_name          = "dev-server"
-  security_group_id = data.aws_security_group.example.id
+  security_group_id = data.aws_security_group.general_dev_server.id
   tag_name          = "c7i-xlarge-benchmark-server"
 }
 
